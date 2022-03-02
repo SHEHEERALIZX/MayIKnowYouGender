c313f52c7811e78547318ac9c9fbda4decba0952
SHEHEER ALI <geekboy@facebook.com>
2022-03-02 18:56:23 +0530

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
 FROM python:3.8
 
-WORKDIR .
+WORKDIR /my-app
 
 COPY requirements.txt .
+
+
+RUN pip install -r requirements.txt
+
+
+COPY . ./my-app
+
+
+CMD ["python","./my-app/main.py"]
 
 
--- main.py
+++ main.py
@@ -1,104 +1,100 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import pymongo
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+# #!/usr/bin/python
+# # -*- coding: utf-8 -*-
+# import pymongo
+# myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
-mydb = myclient['mydatabase']
-mycol = mydb['names']
-
-
-# flag = 1
-
-def UserOpinion(pos, neg, _id):
-
-    userOpinion = input('Do you think this output is correct by choosing Y OR N:')
-
-    if userOpinion == 'y' or userOpinion == 'Y':
-
-        myquery = {'_id': _id}
-        newvalues = {'$set': {'PostiveCount': pos + 1}}
-
-        mycol.update_one(myquery, newvalues)
-    elif userOpinion == 'n' or userOpinion == 'N':
-
-        myquery = {'_id': _id}
-        newvalues = {'$set': {'NegativeCount': neg + 1}}
-
-        mycol.update_one(myquery, newvalues)
+# mydb = myclient['mydatabase']
+# mycol = mydb['names']
 
 
-def genderFinder():
+# # flag = 1
 
-    value = input('Enter your Name: ')
-    name = value.casefold()
+# def UserOpinion(pos, neg, _id):
 
-    x = mycol.find_one({'name': name})
+#     userOpinion = input('Do you think this output is correct by choosing Y OR N:')
 
-    if x is not None:
+#     if userOpinion == 'y' or userOpinion == 'Y':
 
-        pos = x['PostiveCount']
-        neg = x['NegativeCount']
+#         myquery = {'_id': _id}
+#         newvalues = {'$set': {'PostiveCount': pos + 1}}
 
-        print ("NegativeCount",neg,"PositiveCount",pos)
+#         mycol.update_one(myquery, newvalues)
+#     elif userOpinion == 'n' or userOpinion == 'N':
 
-        if neg == 0 and pos == 0 or pos == neg:
+#         myquery = {'_id': _id}
+#         newvalues = {'$set': {'NegativeCount': neg + 1}}
+
+#         mycol.update_one(myquery, newvalues)
 
 
-            if name[-1] == 'a':
-                print (value + ' is a girl')
+# def genderFinder():
 
-                UserOpinion(pos, neg, x['_id'])
-            else:
+#     value = input('Enter your Name: ')
+#     name = value.casefold()
 
-                print (value + ' is a boy')
-                UserOpinion(pos, neg, x['_id'])
+#     x = mycol.find_one({'name': name})
 
-        elif pos > neg :
+#     if x is not None:
 
-            if name[-1] == 'a':
-                print (value + ' is a girl')
-                UserOpinion(pos, neg, x['_id'])
+#         pos = x['PostiveCount']
+#         neg = x['NegativeCount']
 
-            else:
+#         print ("NegativeCount",neg,"PositiveCount",pos)
 
-                print (value + ' is a boy')
-                UserOpinion(pos, neg, x['_id'])    
+#         if neg == 0 and pos == 0 or pos == neg:
 
-        else :
+
+#             if name[-1] == 'a':
+#                 print (value + ' is a girl')
+
+#                 UserOpinion(pos, neg, x['_id'])
+#             else:
+
+#                 print (value + ' is a boy')
+#                 UserOpinion(pos, neg, x['_id'])
+
+#         elif pos > neg :
+
+#             if name[-1] == 'a':
+#                 print (value + ' is a girl')
+#                 UserOpinion(pos, neg, x['_id'])
+
+#             else:
+
+#                 print (value + ' is a boy')
+#                 UserOpinion(pos, neg, x['_id'])    
+
+#         else :
             
-            if name[-1] == 'a':
-                    print (value + ' is a boy')
+#             if name[-1] == 'a':
+#                     print (value + ' is a boy')
 
-                    UserOpinion(pos, neg, x['_id'])
+#                     UserOpinion(pos, neg, x['_id'])
 
-            else:
-                    print (value + ' is a girl')
+#             else:
+#                     print (value + ' is a girl')
 
-                    UserOpinion(neg, pos, x['_id'])    
+#                     UserOpinion(neg, pos, x['_id'])    
 
+#     else:
 
+#         temp = mycol.insert_one({'name': name, 'PostiveCount': 0,
+#                                 'NegativeCount': 0})
 
-            
+#         if name[-1] == 'a':
+#             print (value + ' is a girl')
 
+#             UserOpinion(0, 0, temp.inserted_id)
+#         else:
 
-
-
-
-    else:
-
-        temp = mycol.insert_one({'name': name, 'PostiveCount': 0,
-                                'NegativeCount': 0})
-
-        if name[-1] == 'a':
-            print (value + ' is a girl')
-
-            UserOpinion(0, 0, temp.inserted_id)
-        else:
-
-            print (value + ' is a boy')
-            UserOpinion(0, 0, temp.inserted_id)
+#             print (value + ' is a boy')
+#             UserOpinion(0, 0, temp.inserted_id)
 
 
-genderFinder()
+# genderFinder()
 
 
+
+
+
+print("Hello World")
